b438cd8d543ac3682b3b00ce96f8c5a7de7cb097
ryhara <ryosei.hara@gmail.com>
2024-04-02 11:08:40 +0900

    finish

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Streamlist HandPoseEstimation App
+# Streamlit HandPoseEstimation App
 
+streamlitとmediapipeを用いたHand Pose EstimationのWebアプリケーションです。
+
+
+## Setup
+ローカル環境で動かす場合以下のライブラリが入っていれば動くと思います。
+
+- streamlit
+- mediapipe=0.10.9
+- opencv
+- av
+
+## Usage
+```
+streamlit run src/app.py
+```
+
+## reference
+- [ブラウザで動くリアルタイム画像/音声処理アプリをStreamlitでサクッと作る](https://zenn.dev/whitphx/articles/streamlit-realtime-cv-app)
+- [【Python】Streamlit Sharingで簡単・爆速でWebページをデプロイする！ #Python - Qiita](https://qiita.com/kagami_t/items/c54702e271d729948e24)
---
+++ src/app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from streamlit_webrtc import VideoProcessorBase, webrtc_streamer
+import mediapipe as mp
+import cv2
+import av
+
+
+st.title('Hand Pose Estimation')
+st.write('This app uses MediaPipe to estimate hand pose in real-time.')
+
+thickness = st.slider('Drawing Thickness', min_value=1, max_value=10, value=2, step=1)
+circle_radius = st.slider('Circle Radius', min_value=1, max_value=10, value=2, step=1)
+min_detection_confidence = st.slider('Min Detection Confidence', min_value=0.0, max_value=1.0, value=0.5, step=0.1)
+min_tracking_confidence = st.slider('Min Tracking Confidence', min_value=0.0, max_value=1.0, value=0.3, step=0.1)
+
+
+def callback(frame):
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    image = frame.to_ndarray(format="bgr24")
+    with mp_hands.Hands(
+        min_detection_confidence=min_detection_confidence,
+        min_tracking_confidence=min_tracking_confidence,) as hands:
+        image = cv2.flip(image, 1)
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(
+                    image, hand, mp_hands.HAND_CONNECTIONS,
+                    mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=thickness, circle_radius=circle_radius),
+                    mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=thickness, circle_radius=circle_radius),
+                )
+        return av.VideoFrame.from_ndarray(image, format="bgr24")
+
+
+
+
+webrtc_streamer(key="hand-pose", video_frame_callback=callback, async_processing=True, media_stream_constraints={'video': True, 'audio': False}, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+
+
+st.write("Created by [ryhara](https://github.com/ryhara)")
+st.write("Repository [Streamlit-handpose](https://github.com/ryhara/Streamlit-handpose)")
